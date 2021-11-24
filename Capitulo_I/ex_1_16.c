35933e0c4854ad9567f3a13e31d6e225799e2332
Juan Bolivar <j_bolivar@outlook.com>
2021-11-24 15:48:00 -0500

    until ex 16

---
+++ Capitulo_I/ex_1_16.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <limits.h>
+
+#define MAXLINE 1000000 
+
+
+int getline(char line[] );
+void copy(char to[] , char from[]);
+
+main(){
+  int len;
+  int max;
+
+  char line[MAXLINE];
+  char longest[MAXLINE];
+
+  max=0;
+  while (( len = getline(line)) > 0)
+    if (len > max){
+
+      max = len;
+      copy(longest, line);
+
+    }
+  if( max > 0 )
+    printf("%s \n" , longest);
+
+  return 0;
+
+}
+
+
+int getline(char s[]){
+
+  int c,i;
+  i = 0;
+  while ((c=getchar())!=EOF && c!='\n'){
+       s[i] = c;
+       ++i;
+  }
+  s[i+1] =  c;
+  s[i+2] = '\0';
+  return i;
+
+}
+
+
+
+void copy(char to[] , char from[])
+{
+
+  int i;
+
+  i=0;
+
+  while((to[i] = from[i])!='\0')
+    ++i;
+
+}
