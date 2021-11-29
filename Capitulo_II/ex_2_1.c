284a3241d8c6f752bade7815c39f61ee030969d0
Juan Bolivar <j_bolivar@outlook.com>
2021-11-29 13:13:02 -0500

    ex_2_1.c

---
+++ Capitulo_II/ex_2_1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <limits.h>
+
+
+
+main(){
+
+
+  printf("#######################################\n");
+  printf("CHAR_BIT %d \n", CHAR_BIT);
+  printf("CHAR_MIN %d \n", CHAR_MIN);
+  printf("CHAR_MAX %d \n", CHAR_MAX);
+  printf("UCHAR_MIN %d \n" , 0); 
+  printf("UCHAR_MAX %d \n" , UCHAR_MAX); 
+  printf("SCHAR_MIN %d \n" , SCHAR_MIN); 
+  printf("SCHAR_MAX %d \n" , SCHAR_MAX); 
+  printf("#######################################\n");
+
+ printf("#######################################\n");
+ printf("INT_MIN %d \n", INT_MIN);
+ printf("INT_MAX %d \n", INT_MAX);
+ printf("UINT_MAX %d \n" , UINT_MAX); 
+ printf("#######################################\n");
+  
+ printf("#######################################\n");
+ printf("LONG_MIN %ld \n", LONG_MIN);
+ printf("LONG_MAX %ld \n", LONG_MAX);
+ printf("ULONG_MAX %ld \n" , ULONG_MAX); 
+ printf("#######################################\n");
+
+
+  
+ printf("#######################################\n");
+ printf("SHRT_MIN %d \n", SHRT_MIN);
+ printf("SHRT_MAX %d \n", SHRT_MAX);
+ printf("USHRT_MAX %ld \n" , USHRT_MAX); 
+ printf("#######################################\n");
+ 
+
+  
+  return 0;
+}
